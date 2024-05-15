472c13e8bdcad2568051329b0ff124944b7b22af
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-05-14 22:21:26 -0300

    updates tasks

---
+++ Estrutura_Dados/codeChallengs-5/gerência de uma escola/1.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_ALUNOS 100
+
+//  estrutura aluno
+struct Aluno {
+    int matricula;
+    char nome[50];
+    char curso[50];
+    char endereco[100];
+    bool ativo;
+};
+
+//  funções
+void cadastrarAluno(struct Aluno alunos[], int *numAlunos);
+void removerAluno(struct Aluno alunos[], int numAlunos);
+void alterarCurso(struct Aluno alunos[], int numAlunos);
+void alterarEndereco(struct Aluno alunos[], int numAlunos);
+void relatorioAlunos(struct Aluno alunos[], int numAlunos);
+void mostrarMenu();
+
+int main() {
+    struct Aluno alunos[MAX_ALUNOS];
+    int numAlunos = 0;
+    int opcao;
+
+    do {
+        mostrarMenu();
+        scanf("%d", &opcao);
+        switch(opcao) {
+            case 1:
+                cadastrarAluno(alunos, &numAlunos);
+                break;
+            case 2:
+                removerAluno(alunos, numAlunos);
+                break;
+            case 3:
+                relatorioAlunos(alunos, numAlunos);
+                break;
+            case 4:
+                alterarCurso(alunos, numAlunos);
+                break;
+            case 5:
+                alterarEndereco(alunos, numAlunos);
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
+        }
+    } while(opcao != 0);
+
+    return 0;
+}
+
+void mostrarMenu() {
+    printf("\nMenu:\n");
+    printf("1 - Cadastrar aluno\n");
+    printf("2 - Remover aluno\n");
+    printf("3 - Relatório de alunos\n");
+    printf("4 - Alterar curso\n");
+    printf("5 - Alterar endereço\n");
+    printf("0 - Sair\n");
+    printf("Escolha uma opção: ");
+}
+
+void cadastrarAluno(struct Aluno alunos[], int *numAlunos) {
+    if (*numAlunos < MAX_ALUNOS) {
+        printf("Digite a matrícula: ");
+        scanf("%d", &alunos[*numAlunos].matricula);
+        printf("Digite o nome: ");
+        scanf("%s", alunos[*numAlunos].nome);
+        printf("Digite o curso: ");
+        scanf("%s", alunos[*numAlunos].curso);
+        printf("Digite o endereço: ");
+        scanf("%s", alunos[*numAlunos].endereco);
+        alunos[*numAlunos].ativo = true;
+        (*numAlunos)++;
+        printf("Aluno cadastrado com sucesso.\n");
+    } else {
+        printf("Limite de alunos atingido.\n");
+    }
+}
+
+void removerAluno(struct Aluno alunos[], int numAlunos) {
+    int matricula;
+    printf("Digite a matrícula do aluno a ser removido: ");
+    scanf("%d", &matricula);
+    for (int i = 0; i < numAlunos; i++) {
+        if (alunos[i].matricula == matricula) {
+            alunos[i].ativo = false;
+            printf("Aluno removido com sucesso.\n");
+            return;
+        }
+    }
+    printf("Aluno não encontrado.\n");
+}
+
+void relatorioAlunos(struct Aluno alunos[], int numAlunos) {
+    printf("\nRelatório de alunos:\n");
+    printf("Matrícula\tNome\tCurso\tEndereço\n");
+    for (int i = 0; i < numAlunos; i++) {
+        if (alunos[i].ativo) {
+            printf("%d\t%s\t%s\t%s\n", alunos[i].matricula, alunos[i].nome, alunos[i].curso, alunos[i].endereco);
+        }
+    }
+}
+
+void alterarCurso(struct Aluno alunos[], int numAlunos) {
+    int matricula;
+    char novoCurso[50];
+    printf("Digite a matrícula do aluno: ");
+    scanf("%d", &matricula);
+    printf("Digite o novo curso: ");
+    scanf("%s", novoCurso);
+    for (int i = 0; i < numAlunos; i++) {
+        if (alunos[i].matricula == matricula) {
+            strcpy(alunos[i].curso, novoCurso);
+            printf("Curso alterado com sucesso.\n");
+            return;
+        }
+    }
+    printf("Aluno não encontrado.\n");
+}
+
+void alterarEndereco(struct Aluno alunos[], int numAlunos) {
+    int matricula;
+    char novoEndereco[100];
+    printf("Digite a matrícula do aluno: ");
+    scanf("%d", &matricula);
+    printf("Digite o novo endereço: ");
+    scanf("%s", novoEndereco);
+    for (int i = 0; i < numAlunos; i++) {
+        if (alunos[i].matricula == matricula) {
+            strcpy(alunos[i].endereco, novoEndereco);
+            printf("Endereço alterado com sucesso.\n");
+            return;
+        }
+    }
+    printf("Aluno não encontrado.\n");
+}
--- Estrutura_Dados/codeChallengs-5/readme.md
+++ Estrutura_Dados/codeChallengs-5/readme.md
@@ -21,3 +21,27 @@ A base de dados está em: https://github.com/yurifarod/Algorithms/blob/master/Es
 
 Prazo :
 17 de maio de 2024 às 23:59
+
+
+# tarefa 6
+
+Construa um programa que faça a gerência de uma escola, utilizando sructs.
+Ele deverá :
+* conter o registro de alunos, contendo matricula (inteiro), nome (string), curso (string), endereço (string), ativo (bool);
+* Funções (definidas no código) deverão ser chamadas no menu do programa;
+* Cadastrar aluno (cria um novo aluno e aloca no array de registros;
+* Remover aluno (marca F na variável ativa do aluno);
+* Alterar curso (entra como parâmetro a matricula do aluno e o array de alunos para alterar o seu curso);
+* Alterar endereço (entra como parâmetro a matricula do aluno e o array de alunos para alterar o seu endereço);
+* Relatório de alunos (imprime, de maneira formatada, todos os alunos ATIVOS e seus dados;
+* O menu não precisará de interface gráfica, podemos usar uma mensage de texto que peça um número para cada uma das possiblidades ( 1 – Cadastrar, 2 – Remover, 3 – Relatório, etc);
+* Número 0 é digitado para sair do programa;
+* Um número que seja digitado e não esteja no menu deverá imprimir  a mensagem “Opção Inválida” e chamar o menu novamente;
+* Após cada operação, até que seja chamada a opção 0 (sair) o Menu deverá ser impresso novamente.
+
+
+Material utilizado como referência:
+
+A base de dados está em: https://github.com/yurifarod/Algorithms/tree/master/Estrutura%20de%20Dados
+
+
